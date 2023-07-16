a53bc4a3431ed5a6c647df993e29b49f06e94823
Bbaranoski <breno.b.o.baranoski@gmail.com>
2023-07-16 14:09:16 -0300

    make the wincondition functio

--- javascript.js
+++ javascript.js
@@ -1,10 +1,18 @@
 const Gameboard = (() => {
     const gameBoard = ['', '', '', '', '', '', '', '', '']
 
+    const placeMarker = (place, marker) =>{
+        gameBoard[place] = marker
+        render()
+    }
+
     const render = () => {
-            gameBoard.forEach((e, i) => {
+        gameBoard.forEach((e, i) => {
+
             const selectP = select(i)
             selectP.textContent = e
+            winCondition()
+
         })
     }
 
@@ -12,9 +20,8 @@ const Gameboard = (() => {
         return document.getElementById(`${index}`)
     }
 
-    const placeMarker = (place, marker) =>{
-        gameBoard[place] = marker
-        render()
+    const winCondition = () =>{
+
     }
 
     return{placeMarker}
@@ -26,25 +33,23 @@ const DisplayControlle = (() => {
     let turnOne = true
     const divs = document.querySelectorAll('div > div')
 
-    divs.forEach((div) =>{
-        
+    divs.forEach((div) =>{  
         div.addEventListener('click', () => {
 
             if(turnOne == true && div.className != 'marked'){
-    
+
                 Gameboard.placeMarker(div.className, playerOne.marker)
                 div.className = 'marked'
                 turnOne = false
 
             }else if(div.className != 'marked'){
-                
+
                 Gameboard.placeMarker(div.className, playerTwo.marker)
                 div.className = 'marked'
                 turnOne = true
 
             }   
-    
-       })
+       }) 
     })
 
 })()
